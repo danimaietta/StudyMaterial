da5f67f536ff253e4d0ce70f2fd5f74d815e1d03
Daniel Maietta Esquivel <danimaietta1@gmail.com>
2021-01-13 23:30:05 -0600

    lifecycle finished

--- React/Lifecycle/app/components/Lifecycle.js
+++ React/Lifecycle/app/components/Lifecycle.js
@@ -4,19 +4,83 @@ export default class Lifecycle extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            stat: 'state'
+            message: 'state'
+        }
+        console.log("constructor: this happens before anything else")
+    }
+
+    //warning
+    /*
+        Before the component is mounted
+    */
+    componentWillMount(){
+        console.log('componentWillMount')
+    }
+
+    /*
+        After the component is mounted
+    */
+    componentDidMount(){
+        console.log('componentDidMount')
+    }
+
+    /*
+        This ask if you can update the component or not by returning true or false
+        Helps to validate in cases the component doesn't need to be updated
+    */
+    shouldComponentUpdate(prevProps, nextState){
+        console.log('shouldComponentUpdate ' + prevProps.foo)
+        return true
+    }
+
+    //warning
+    /*
+        Before the component updates you can validate what you want with the new state
+        Check if the new state match what you want
+    */
+    componentWillUpdate(nextProps, nextState){
+        if(nextState.message === 'new state'){
+            console.log('componentWillUpdate to: ' + nextState.message)
         }
     }
 
-    componentDidMount(){
-        console.log('this happens first')
-    }
-
-    componentWillUnmount(){
-        console.log('this happens last')
-    }
-
     render(){
-        return <h1>Hello World {this.state.stat}</h1>   
+        console.log("Im rendering right now")
+        return (
+            <>
+                <h1>Lifecycle {this.state.message}</h1>
+                <button onClick={() => this.setState({message: 'new state'})}>Update State</button>
+            </>
+        )
     }
+
+    //warning
+    /*
+        Everytime a component updates the state it enters in this method
+        1st argument is previous props and 2nd is previous state
+        Always make a statement to validate what you need
+    */
+    componentDidUpdate(prevProps, prevState){
+        if (prevState.message !== this.state.message) {
+            console.log('componentDidUpdate: state.message changed')
+        }else if(prevProps.foo === this.props.foo){
+            console.log('componentDidUpdate: state.props.foo didnt changed ')
+        }
+    }
+
+    /*
+        When the component is going to be removed from the DOM
+    */
+    componentWillUnmount(){
+        console.log('componentWillUnmount')
+    }
+
+    //warning
+    /*
+        When the component receive different props it enters here
+    */
+    componentWillReceiveProps(nextProps){
+        console.log('componentWillReceiveProps: ' + nextProps.foo)
+    }
+
 }
--- React/Lifecycle/app/index.js
+++ React/Lifecycle/app/index.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Lifecycle from './components/Lifecycle'
+import HelloWorld from './components/HelloWorld'
 
 function App() { 
+
+  let [life, setLife] = useState(true)
+  let [num, setNum] = useState(123)
+
   return (
-    <Lifecycle/>
+    <>
+      {
+        life ?
+          <Lifecycle foo={num}/>
+        :
+          <HelloWorld />
+      }
+      <br/>
+      <button onClick={() => setLife(!life)}>Toggle Components</button>
+      <br/>
+      <button onClick={() => setNum(456)}>Send Different Props to Lifecycle Component</button>
+    </>
   )
 }
 
