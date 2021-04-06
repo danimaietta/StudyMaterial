bb5df1c7a201ec0a7a905192342906290f6f1d02
Daniel Maietta Esquivel <danimaietta1@gmail.com>
2021-04-06 16:21:24 -0600

    finished useRef hook

--- Hooks/Ref/app/components/Email.js
+++ Hooks/Ref/app/components/Email.js
@@ -3,16 +3,17 @@ import React, { useState, useRef } from 'react'
 export default function Email() {
   const [renderMe, setRenderMe] = useState(false)
   const input = useRef('')
+  let normalVariable = 0
 
   function handleSubmit() {
+    normalVariable = normalVariable + 1
     console.log('The email is ' + input.current.value)
+    console.log('The normalVariable is ' + normalVariable)
   }
-
-  console.count('Email rendered')
 
   return (
     <>
-      <h4>{input.current.value}</h4>
+      <h4>Input Value: {input.current.value}</h4>
       <input ref={input} type='text' />
       <button onClick={handleSubmit}>Submit</button>
       <button onClick={() => setRenderMe(!renderMe)}>Render</button>
