883c5965c8e36ffb763b43074538240f7d459b09
Daniel Maietta Esquivel <danimaietta1@gmail.com>
2021-09-04 20:47:24 -0600

    Testing folder rearrenge

--- React/Router/app/index.js
+++ React/Router/app/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import ClassFather from './components/classComponents/ClassFather'
 import FunctionFather from './components/functionComponents/FunctionFather'
 
+// ClassFather and FunctionFather contain its own router
 function App() {
   return (
     <>
