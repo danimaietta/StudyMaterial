690175d1097ac57e297b14015097e7c3fec2ecb9
Daniel Maietta Esquivel <danimaietta1@gmail.com>
2022-01-26 03:28:02 -0600

    todolist functionalities finished, display info dynamic is missing

--- Redux/Example/src/ToDoList.js
+++ Redux/Example/src/ToDoList.js
@@ -1,29 +1,41 @@
-import React from 'react'
+import React, { useState, useCallback, useRef } from 'react'
 import { useStore, useDispatch } from 'react-redux'
 
 export function ToDoList() {
   const store = useStore()
   const dispatch = useDispatch()
-
-  console.log(store.getState().toDoListReducer)
+  const input = useRef('')
+  const [, updateState] = useState()
+  const forceUpdate = useCallback(() => updateState({}), [])
 
   return (
     <div>
+      <input ref={input}></input>
       <ul>
         {store.getState().toDoListReducer.map((item, index) => (
           <li key={index}>
             {item}
             <button
-              className='add-btn'
-              onClick={() => dispatch({ type: 'ADD_ITEM', value: 'hello world' })}
+              className='update-btn'
+              onClick={() =>
+                dispatch({ type: 'UPDATE_ITEM', value: input.current.value, index })
+              }
             >
-              +
+              #
             </button>
-            <button className='update-btn'>#</button>
-            <button className='delete-btn'>-</button>
+            <button
+              className='delete-btn'
+              onClick={() => dispatch({ type: 'DELETE_ITEM', index })}
+            >
+              -
+            </button>
           </li>
         ))}
       </ul>
+      <button onClick={() => dispatch({ type: 'ADD_ITEM', value: input.current.value })}>
+        +
+      </button>
+      <button onClick={forceUpdate}>Update me</button>
     </div>
   )
 }
--- Redux/Example/src/store.js
+++ Redux/Example/src/store.js
@@ -5,26 +5,28 @@ const initalState1 = ['first item', 'second item']
 function toDoListReducer(state = initalState1, action) {
   switch (action.type) {
     case 'ADD_ITEM':
-      console.log([...state, action.value])
       return [...state, action.value]
+    case 'UPDATE_ITEM':
+      state[action.index] = action.value
+      return [...state]
+    case 'DELETE_ITEM':
+      return state.filter((v, i) => i !== action.index)
     default:
       return [...state]
   }
 }
 
 const initalState2 = {
-  counter: 0
+  inputValue: ''
 }
 
-function reducer2(state = initalState2, action) {
+function inputReducer(state = initalState2, action) {
   switch (action.type) {
     case 'INCREMENT':
-      return { ...state, counter: state.counter + 1 }
-    case 'DECREMENT':
-      return { ...state, counter: state.counter - 1 }
+      return [...state, 1]
     default:
       return state
   }
 }
 
-export const store = createStore(combineReducers({ toDoListReducer, reducer2 }))
+export const store = createStore(combineReducers({ toDoListReducer, inputReducer }))
