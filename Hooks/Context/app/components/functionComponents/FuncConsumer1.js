b602b5dacbe3a0115f5f208c6a1899aa27b9f1a8
danimaietta <danimaietta1@gmail.com>
2022-03-03 03:31:24 -0600

    added an obj in useContext

--- Hooks/Context/app/components/functionComponents/FuncConsumer1.js
+++ Hooks/Context/app/components/functionComponents/FuncConsumer1.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import LocaleContext from '../context/LocaleContext'
 
 export default function FuncConsumer1() {
-  const locale = useContext(LocaleContext)
-  return <> { locale == 'en' ? <h4>I'm consumer 1</h4> : <h4>Soy consumidor 1</h4> } </>
+  const { locale, obj } = useContext(LocaleContext)
+  console.log(obj.name)
+  return <> {locale == 'en' ? <h4>I'm consumer 1</h4> : <h4>Soy consumidor 1</h4>} </>
 }
--- Hooks/Context/app/components/functionComponents/FunctionProviderComponent.js
+++ Hooks/Context/app/components/functionComponents/FunctionProviderComponent.js
@@ -4,25 +4,26 @@ import FuncConsumer1 from './FuncConsumer1'
 import FuncConsumer2 from './FuncConsumer2'
 import FuncConsumer3 from './FuncConsumer3'
 
-export default function FunctionProviderComponent(){
-  
+export default function FunctionProviderComponent() {
   const [locale, setLocale] = useState('en')
 
   const toggleLocale = () => {
     locale == 'en' ? setLocale('es') : setLocale('en')
   }
 
+  const obj = { name: 'dani' }
+
   return (
-    <LocaleContext.Provider value={locale}>
-      { locale == 'en' ? 
-        <h1>I'm a function component</h1> : 
+    <LocaleContext.Provider value={{ locale, obj }}>
+      {locale == 'en' ? (
+        <h1>I'm a function component</h1>
+      ) : (
         <h1>Soy el componente de función</h1>
-      }
+      )}
       <FuncConsumer1 />
       <FuncConsumer2 />
       <FuncConsumer3 />
       <button onClick={() => toggleLocale()}> Toggle locale </button>
     </LocaleContext.Provider>
-  )  
-
+  )
 }
