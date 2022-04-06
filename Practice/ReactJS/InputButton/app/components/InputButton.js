80d17adb47c81928c3b014dc597b49226640d7c7
danimaietta <danimaietta1@gmail.com>
2022-04-05 21:37:08 -0600

    React inputButton example added

---
+++ Practice/ReactJS/InputButton/app/components/InputButton.js
@@ -0,0 +1,13 @@
+import React, { useState, useRef } from "react"
+
+export default function InputButton() {
+  const [value, setValue] = useState("hi")
+  const inputRef = useRef("")
+  return (
+    <div>
+      <h1>{value}</h1>
+      <input ref={inputRef} />
+      <button onClick={e => setValue(inputRef.current.value)}>Press me</button>
+    </div>
+  )
+}
---
+++ Practice/ReactJS/InputButton/app/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import InputButton from "./components/InputButton"
+
+function App() {
+  return <InputButton />
+}
+
+ReactDOM.render(<App />, document.getElementById("app"))
+
