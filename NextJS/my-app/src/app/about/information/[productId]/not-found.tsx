fc81faa7f47a2d96d955e9aac3ba52f4257d6a78
danimaietta <danimaietta1@gmail.com>
2024-01-21 15:50:30 -0600

    Nextjs project, notfound, routing

---
+++ NextJS/my-app/src/app/about/information/[productId]/not-found.tsx
@@ -0,0 +1,6 @@
+export default function NotFound() {
+    return (
+      <h1>Product not found :(</h1>
+    )
+}
+  
---
+++ NextJS/my-app/src/app/about/information/[productId]/page.tsx
@@ -0,0 +1,11 @@
+import  { notFound } from "next/navigation"
+
+export default function productId({ params }: {
+    params: { productId: number }
+}) {
+    params.productId > 100 && notFound()
+    return (
+      <h1>Product: {params.productId} </h1>
+    )
+}
+  
