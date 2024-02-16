15af1e8e02a36de7e2f528c10fa639d993fa7ae8
danimaietta <danimaietta1@gmail.com>
2024-02-16 03:30:02 -0600

    get post delete api with products and orders

---
+++ Node/REST-API/api/routes/orders.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET request to /products'
+    })
+})
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Order was created'
+    })
+})
+
+router.get('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order details',
+        orderId: req.params.orderId
+    })
+})
+
+module.exports = router;
---
+++ Node/REST-API/api/routes/products.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET request to /products'
+    })
+})
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling POST request to /products'
+    })
+})
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if(id === 'special'){
+        res.status(200).json({
+            message: 'You discovered the special ID'
+        })
+    } else {
+        res.status(200).json({
+            message: `You are using id ${id}`
+        })
+    }
+})
+
+router.patch('/:productId', (req, res, next) => {
+    const { productId } = req.params;
+    return res.status(200).json({
+        message: `Updated product: ${productId}`
+    })
+})
+
+router.delete('/:productId', (req, res, next) => {
+    const { productId } = req.params;
+    res.status(200).json({
+        message: `Deleted product: ${productId}`
+    })
+})
+
+module.exports = router;
