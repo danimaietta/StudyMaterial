a2d7b27e763bf0fa064a5cba3ba7026cab50706e
danimaietta <danimaietta1@gmail.com>
2024-03-29 05:59:40 -0600

    get, post, patch working

--- Node/Challenge1/routes/datas.js
+++ Node/Challenge1/routes/datas.js
@@ -1,22 +1,55 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
+let currentArray = [];
+
+router.get('/data', (req, res) => {
+    currentArray.sort((a, b) => a - b)
+
+    console.log('currentArray:', currentArray)
+
     res.status(200).json({
-        message: 'Handling GET request to /data'
+        numbers: currentArray
     })
 })
 
 router.post('/data', (req, res) => {
     const numbers = req.body
 
-    console.log(numbers)
-
-    if (numbers.length !== 500) {
-        throw new Error('Invalid numbers length, must be 500')
+    // Check if the input is an array
+    if(!numbers.length) {
+        throw new Error('Invalid input, must be an array')
+    }
+    
+    // Check if the array has 500 elements
+    if (numbers.length !== 100) {
+        throw new Error('Invalid numbers length, must be 100')
     } 
 
+    // Check if the array contains only numbers
+    numbers.forEach(item => {
+        if (typeof item !== 'number') {
+            throw new Error('Invalid input, must be a number')
+        }
+    })
+
+    currentArray = numbers
+    
     res.json('Hello World')
+})
+
+router.patch('/data/:newNumber', (req, res) => {
+    const newNumber = req.params.newNumber
+    try {
+
+        // Perform the patch operation on the currentArray
+        currentArray.push(+newNumber)
+
+        res.json('Data patched successfully')
+        res.status(200).json('Data patched successfully')
+    } catch (error) {
+        res.status(400).json(error.message)
+    }
 })
 
 module.exports = router;
