be8f0d83fc2139f3fc421b1672c65754d11f4863
danimaietta <danimaietta1@gmail.com>
2024-04-24 14:19:47 -0600

    WithoutMultiThread example done

---
+++ Node/WithoutMultiThread/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/non-blocking/', (req, res) => {
+    res.status(200).send('Non-blocking request');
+});
+
+app.get('/blocking', (req, res) => {
+    let sum = 0;
+    for (let i = 0; i < 1000000000; i++) {
+        sum += i;
+    }
+    res.status(200).send('Blocking request');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
